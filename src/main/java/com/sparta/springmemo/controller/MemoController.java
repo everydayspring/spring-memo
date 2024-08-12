f8d4a7a330060158b46fc6cca8786c131c848936
everydayspring <lee.bom.952@gmail.com>
2024-08-12 15:11:09 +0900

    Create Read 구현

---
+++ src/main/java/com/sparta/springmemo/controller/MemoController.java
@@ -0,0 +1,51 @@
+package com.sparta.springmemo.controller;
+
+import com.sparta.springmemo.dto.MemoRequestDto;
+import com.sparta.springmemo.dto.MemoResponseDto;
+import com.sparta.springmemo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        // RequestDto -> Entity
+        Memo memo = new Memo(requestDto);
+
+        // Memo Max ID check
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+
+        // DB 저장
+        memoList.put(memo.getId(), memo);
+
+        // Entity -> ResponseDto
+        MemoResponseDto responseDto = new MemoResponseDto(memo);
+
+        return responseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        // Map To List
+        List<MemoResponseDto> responseList = memoList.values().stream()
+                .map(MemoResponseDto::new).toList();
+
+        return responseList;
+    }
+}
+
+
+
+
+
+
