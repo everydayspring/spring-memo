294c9efbbe8491f6a253a56e4f6df0f4a3c8cea4
everydayspring <lee.bom.952@gmail.com>
2024-08-18 23:43:15 +0900

    JPA Query Method

--- src/main/java/com/sparta/springmemo/repository/MemoRepository.java
+++ src/main/java/com/sparta/springmemo/repository/MemoRepository.java
@@ -3,6 +3,10 @@ package com.sparta.springmemo.repository;
 import com.sparta.springmemo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {
+import java.util.List;
 
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    List<Memo> findAllByUsername(String username);
 }
